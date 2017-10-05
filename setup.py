a4cf09dfb4e468084cb4988fe5e9e83e4a2ad8a4
Matteo Cafasso <noxdafox@gmail.com>
2017-10-05 23:42:30 +0300

    Release 0.0.7
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read(fname):
 
 setup(
     name="clipspy",
-    version="0.0.4",
+    version="0.0.7",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("CLIPS Python bindings"),
