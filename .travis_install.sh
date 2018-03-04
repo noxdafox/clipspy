9baa6b98cd9a60f814f60977c12c8c71695e8465
Matteo Cafasso <noxdafox@gmail.com>
2018-03-04 12:54:24 +0200

    travis: fix test environment installation
    The CLIPS source zip seems corrupted.
    
    Fetch CLIPS package from Debian Unstable.
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-WORK_DIR=$(pwd)
-CLIPS_DIR=$WORK_DIR/clips_core_source_630
-DOWNLOAD_URL="https://downloads.sourceforge.net/project/clipsrules/CLIPS/6.30/clips_core_source_630.zip"
+UBUNTU_XENIAL="deb http://archive.ubuntu.com/ubuntu xenial main"
+DEBIAN_UNSTABLE="deb http://ftp.se.debian.org/debian/ sid main"
 
-wget -O clips.zip $DOWNLOAD_URL
+# https://bugs.launchpad.net/ubuntu/+source/dpkg/+bug/1730627
+echo $UBUNTU_XENIAL | sudo tee /etc/apt/sources.list.d/ubuntu_xenial.list
+sudo apt update
+sudo apt install -o APT::Force-LoopBreak=1 dpkg libc6
 
-unzip clips.zip
-
-cd $CLIPS_DIR
-
-cp makefiles/makefile.lib core/Makefile
-
-cd $CLIPS_DIR/core/
-
-sed -i 's/gcc -c/gcc -fPIC -c/g' Makefile
-
-make
-
-ld -G *.o -o libclips.so
-
-cd $WORK_DIR
+echo $DEBIAN_UNSTABLE | sudo tee /etc/apt/sources.list.d/debian_unstable.list
+sudo apt-get --allow-unauthenticated update
+sudo apt install -y --allow-unauthenticated -t sid libclips libclips-dev
 
 sudo pip install --upgrade pip
 sudo pip install --upgrade setuptools
 sudo pip install --upgrade pytest
 
-python setup.py build_ext --include-dirs $CLIPS_DIR/core/ --library-dirs $CLIPS_DIR/core/
-python setup.py install
-
-CFLAGS="-I$CLIPS_DIR/core/" LDFLAGS="-L$CLIPS_DIR/core/" python clips/clips_build.py
-
-mv _clips* clips/
+pip install .
