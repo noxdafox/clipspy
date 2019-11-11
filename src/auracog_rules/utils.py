470ed795f0af2313d55191a043dfdc405be013c4
jesusdelpeso <df382ef8615a9b3fdd2d1343267c3f14edce82d8>
2019-11-11 15:20:24 +0100

    Simplified

--- src/auracog_rules/utils.py
+++ src/auracog_rules/utils.py
@@ -1,4 +1,5 @@
 from typing import Text, Callable, List
+import importlib
 
 def get_functions_from_module_name(module_name: Text) -> List[Callable]:
     """
@@ -8,21 +9,10 @@ def get_functions_from_module_name(module_name: Text) -> List[Callable]:
 
     :return: A list with the functions found. The empty list if no function is found. Only functions are returned.
     """
-    module = __import__(module_name)
+
+    module = importlib.import_module(module_name)
     res = []
     for name in dir(module):
-        if is_user_function(module, name):
+        if not(name.startswith("__") and name.endswith("__")) and callable(eval("module.{}".format(name))):
             res.append(eval("module.{}".format(name)))
     return res
-
-def is_user_function(module, name: Text) -> bool:
-    """
-    Get whether a
-    :param module:
-    :param name:
-    :return:
-    """
-
-    if name.startswith("__") and name.endswith("__"):
-        return False
-    return callable(eval("module.{}".format(name)))
