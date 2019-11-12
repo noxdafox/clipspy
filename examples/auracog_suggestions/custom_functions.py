cc0c4941f2b35c92dd951025e9dd2d0a31c67437
jesusdelpeso <df382ef8615a9b3fdd2d1343267c3f14edce82d8>
2019-11-12 18:01:43 +0100

    Custom functions used for testing.

---
+++ examples/auracog_suggestions/custom_functions.py
@@ -0,0 +1,10 @@
+# This module contains the python functions to be invoked from rules.
+#
+
+def print_test(s):
+    """
+    This is a test function, used jst to check that calling a python function from clipspy is working.
+
+    :param s:
+    """
+    print(s)
